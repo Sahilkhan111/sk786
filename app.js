25030327190e4a709b91a975894b2ca35cd61c1d
Sahilkhan111 <134552773+Sahilkhan111@users.noreply.github.com>
2023-09-24 07:49:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,810 @@
+//    CHAPTER 1:
+
+// Question 1:
+// var FirstName="Sahil";
+// alert(FirstName);
+// var LastName="Khan";
+// alert(LastName);
+// var Email="Khan.222@gmail.com";
+// alert(Email);
+// var PhoneNumber= "03021589390";
+// alert(PhoneNumber);
+// var pasword="1122334455";
+// alert(pasword);
+
+// Question 2:
+// alert("You are ! learning JavaScript");
+
+
+// Question 3:
+// var message="GOD KNOEWS BETTER";
+// alert(message);
+
+
+
+//    CHAPTER 2:
+
+// Question 1:
+// let myVariableName="SahilKhan";
+// alert(myVariableName);
+
+//Question 2:
+// var Country;
+// Country="USA";
+// alert(Country);
+
+// //Question 3:
+//  var TeamName="Pakistan";
+//  alert(TeamName);
+
+// //Question 4:
+//  var bestMan="BABR AZAM" ;
+//  alert(bestMan); 
+
+
+ //    CHAPTER 3:
+//Quwstion 1&2:
+//  var caseQt;
+//  caseQty=144;
+// alert(caseQty);
+
+
+// //Question 4:
+// var sum;
+// sum=2+3;
+// document.write(sum);
+
+// //Question 5:
+// var merchTotal = 590;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+
+
+// //Quwstion 6:
+// var num=10;
+// num=num+9;
+// alert(num);
+
+// Chapter 4 (Variable names Legal and  Illegal)
+
+// 1. Correct this statement.
+// var product cost = 3.45;
+
+// var productcost =3.45;
+// console.log(productcost);
+
+
+// 2. Rewrite this using camelCase.
+// var Nameofband;
+
+/*var nameOfBand ="1";
+alert (nameOfBand)*/
+
+// 3. In a single statement declare a legally-named variable and assign a 
+// number to it.
+
+/*var myVariable = 2;
+document.write(myVariable);*/
+
+
+// 4. Declare a variable that is a combination of your first and last 
+// names. Use camelCase.
+
+/* var  firstName = "Muhammad";
+var lastName ="Huzaifa";
+var fullName = firstName + " " + lastName ;
+
+alert(fullName);*/
+
+
+
+// 5. List the legal and Illegal Variables.
+
+
+// ********** legal variables ************
+
+/*var a = "a to z";
+var myVariable = "camelCase";
+var abc123 = "abc 123";
+// alert (abc123);
+var _variable = "with underScor";
+// alert(_variable);
+var first_name = "huzaifa";
+// alert(first_name);*/
+
+
+
+
+// ********** illegal variables ************
+
+/*var first name = "due to gap";
+var -variable = " - ";
+var @gmail = "no special character";
+var #$&/abc = "no special characters";
+var .name = "";
+var 1num = " start with number Or only number variable";
+*/
+
+
+
+
+
+// Chapter 5 (Math Expression I)
+
+// 1. What is the name and symbol of the arithmetic operator that 
+// gives you the remainder when one number is divided by another?
+
+
+
+
+
+// 2. What is the value of num?
+// var num = 20 % 6;
+
+
+// 3. In a single statement, declare the variable largeNum and assign it 
+// the result of 1,000 multiplied by 2,000
+
+// var largeNum = 1000 * 2000;
+// alert (largeNum);
+
+// 4. Assign to a variable the value represented by one variable 
+// subtracted from the value represented by another variable
+
+// var a = 2;
+// var b = a - 5;
+// alert (b); 
+
+
+// 5. Assign to a variable the remainder when one number is divided by 
+// another. The variable hasn't been declared beforehand. Make up 
+// the variable name
+
+
+
+// 6. Code an alert that displays the result of a multiplication on 2 
+// numbers.
+
+// alert(2*10);
+
+
+// Chapter 6 (Math Expression II)
+
+// 1. Code a short form of x = x + 1; Use either of the two legal 
+// expressions.
+
+// x= 9;
+// x = x +1;
+// console.log(x);
+
+// 2. If x has a value of 100, what is the fastest way to reduce it to 99 
+// with a math expression?
+
+
+// x= 100;
+// y= --x ;
+// alert (x);
+
+// 3. var x = 50;
+// var y = x++;
+// What is the value of y?
+
+// var x= 50;
+// var y = x++;
+// alert (y);
+
+
+// 4. var y = 50;
+// var z = --y;
+// What is the value of z?
+
+
+// var y = 50 ;
+// var z = --y;
+// alert(z);
+// 49
+// the value of z = -1;
+
+
+// 5. In a single statement, decrement num and assign its original value 
+// to newNum.
+
+// var num = 1;
+// var newNum = --num;
+// alert(newNum);
+
+
+// 6. In a single statement add 1 to a variable and assign its original 
+// value to another variable.
+
+// var a = +1;
+// var y = a;
+
+// alert(y);
+
+// 7. Assign a number value to a variable.
+// Increment the variable.
+// Display the new value in an alert
+
+// var num = 10;
+// var a = ++num;
+// alert(a);
+
+
+// Chapter 7 (Math Expression III)
+
+
+// 1. var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+
+//  var calculatedNum = 2 + (2 * 6);
+//  alert(calculatedNum);
+
+//  ans = 14;
+
+// 2. var calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+
+// var calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum);
+// ans =24;
+
+// 3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+
+//  var calculatedNum = (2 + 2) * (4 + 2);
+//  alert(calculatedNum);
+// ans = 24;
+
+// 4. var calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+
+// alert(calculatedNum);
+// ans = 18;
+
+// Note: Try all the above equations yourself.
+// 5. Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, 
+// clarified with parentheses, producing 56.
+
+
+
+// 6. Write a statement that assigns to units the result of 2 + 2 * 4 + 10, 
+// clarified with parentheses, producing 20
+
+// var  result = (2 + 2) * (4 + 10);
+
+// 7. Write a statement that assigns to pressure the result of 4 / 2 * 4, 
+// clarified with parentheses, producing 5
+
+//  var result = (4 / (2 * 4));
+//  alert (result);
+
+// Chapter 8 (Concatenating Text 
+// Strings)
+
+
+// 1. var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+
+//  var num = "2" + "2";
+// alert (num);
+// 22
+
+// 2. message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+// Alert the statement
+
+// message = ("Hello," + "Dolly");
+
+// alert ( message);
+// Hello,Dolly;
+
+
+// 3. alert("33" + 3);
+// What message displays in the alert box
+
+// alert("33" + 3);
+// its 333
+
+
+// 4. Write an alert that displays the concatenation of the two parts of 
+// "Pakistan Zindabad"
+
+
+// alert ( "Pakistan" + "Zindabad")
+
+// PakistanZindabad;
+
+// 5. Write a statement that assigns to a variable the concatenation of 
+// a string with a number
+
+// var concatenation = "2" +5;
+// alert (concatenation);
+// 25
+
+
+// 6. Assign strings to two variables. Then concatenate them and assign 
+// the result to a third variable
+
+// var a = "5";
+// var b = "5";
+// var c = a+ b;
+// alert(c);
+// 55
+
+// Chapter 9 (Prompts)
+
+
+// 1. Code a prompt with the message "Enter first name". The user's 
+// response is assigned to firstName.
+
+//  var firstName = prompt  ("Enter first name");
+
+// 2. Code a prompt with the message "Country?" and the default 
+// answer "China". The user's response is assigned to country
+
+// var country = prompt ("Country?" , "China");
+
+// 3. Correct this statement
+// var yourName = prompt(Enter Your Name");
+
+// var yourName = prompt ("Enter Your Name");
+
+// 4. Code a prompt that specifies a string as the message Include a 
+// default input
+
+// var userName = prompt("Enter Your Name " , "hey!");
+
+
+// 5. Assign strings to two variables. Code a prompt specifying the first 
+// variable as the message and the second variable as the default 
+// response. Assign the user's response to a third variable.
+
+// var a = "5";
+// var b = "10";
+
+// var c = prompt("5" , b);
+
+// var userResponse = c;
+
+// document.write(userResponse);
+
+
+// 6. Display a prompt, including both a message and a default 
+// response.
+// Display the user's response in an alert
+
+// var a = prompt ("message" , "message");
+
+// alert (a);
+
+// Chapter 10 (if statments)
+
+// 1. var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// Correct the above statement:
+// Also try this statement by yourself
+
+
+// var city = "Karachi"
+//  if (city = "Karachi") {
+//  console.log("The City OF Lights")};
+
+
+// 2. This is the first line of an if statement:
+// if (x === y) {
+// Complete the statement. If the condition is true, display a box 
+// that asks the user value of z? and assign it to another variable
+
+// x= 5;
+// y = 5;
+// if (x === y) {
+//     var z = prompt("Enter the value of z");
+// }
+
+// 3. Code an if statement that tests if ZipCode is "10010" so, Alert 
+// that "Karachi". if not then alert ("Please write correct city")
+
+// var zipCode = prompt("Enter zipcode.. ");
+// if (zipCode == "10010"){
+//     alert("Karachi")
+// }else{
+//     alert ("Please write correct city");
+// }
+
+// 4. Code an if statement. Test whether a variable has a particular 
+// numerical value. If so, assign a new value to that variable, as in 
+// x = 1;
+// var x=5;
+// if(x===5){
+//     x=1;
+//     alert(x);
+// }
+
+
+//CHAPTER 11:
+
+
+// Question1:
+// var x = 1;
+//  var y=5;
+//  if(x!=y){
+     
+//      alert("Not Equal to");
+//  }
+
+// Question2:
+//  var x = 10;
+//   var y=5;
+//   if(x>=y){
+     
+//       alert("ok");
+//   }
+
+// Question3:
+//  var x = 10;
+//    if(x!=11){
+     
+//        alert("ok");
+//    }
+
+// Question4:
+
+//  var x = 1;
+//   var y=5;
+//   if(x!=y){
+     
+//     alert("congratulations ");
+//   }
+
+// Question5:
+
+// var userNane = prompt("your first name.");
+// if(userNane != "ali" ){
+//     alert("no match");
+// }
+
+// Chapter 12 
+// question 1
+// var num = 5;
+// var num2 = 10;
+// if (num >= num2){
+//     alert("work")
+// }else{
+//     alert("not work")
+// }
+
+// question 2
+
+// var Marks = prompt("enter marks");
+// var TotalMarks=prompt("Enter total Marks")
+// var precentage= Marks/TotalMarks*100;
+// if(precentage>=70){
+//      alert("your grade is : A");
+//  }
+//  else if(precentage >=69){
+//     alert("Yor grade is : B");
+//  }
+//  else if(precentage>=50){
+//     alert("Your Grade is C");
+//  } 
+//  else{
+//      alert("Your grade is F");
+//  }
+
+// Question 3:
+// var a=14;
+// if (a === 10) {
+//     alert("a is 10");
+//   }
+//   else{
+//     alert("a is :" +a);
+//   }
+
+// question = 4
+
+// var userCity = prompt("Enter City");
+// if (userCity == "karachi"){
+//     alert("it is karachi");
+// } else if (userCity == "lahore"){
+//     alert("its lahore");
+// }else{
+//     alert("no city found");
+// }
+  
+// Chapter 13
+
+// question 1
+
+// var a = 5;
+// var b = 5;
+// var c = 5;
+// var d = 5;
+// if(a==b,c==d){
+//     alert("true");
+// }
+
+// question 2
+
+// var a = 5;
+//   var b = 5;
+//   var c = 6;
+//   var d = 5;
+
+//  if(a==b,c!=d){
+//          alert("true");
+//  }
+
+// question 3
+// var name="Hamza";
+// var age =40;
+// if(name=="Hamza","Arslan", age<60){
+// alert("OK");
+// }
+
+// Question4
+// var num1=5;
+// var num2=6;
+// if(num1<num2){
+//     alert("greater")
+// }
+// else{
+//     alert("lesser")
+// }
+
+
+// Question5
+
+// var FirstName="Ali";
+// var LastName="Ahmed";
+// var CheckFirstName=prompt("Enter first Name");
+// var CheckLastName=prompt("Enter last Name");
+// if(FirstName==CheckFirstName,LastName==CheckLastName){
+//     alert("match");
+// }
+// else{
+//     alert(" NO match");
+// }
+
+
+// Chapter14
+// Quesrion1
+//  var password = prompt("Your Pasword:"); 
+//   if (password !== "") {
+//          if (password.length <= 5) {
+//              alert("Password must be greater than 5");
+
+//  } else {
+
+//         alert("OK");
+
+//     }
+//  } else {
+
+//        alert("Password cannot be empty");
+
+// }
+//Question2:
+// var a=1;
+// var c="Max";
+// if (a === 1) {
+//  if (c === "Max") {
+//     alert("OK");
+//   }
+// }
+
+
+//Question3:
+// var a=1;
+// var c="Max";
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+// }
+
+//Question4:
+// var a=2;
+// var b= 2;
+// if(a==b){
+//     if(a<=b){
+//         alert("Answer is right");
+//     }
+// }
+
+
+
+
+
+//Chapter 21:
+//Question1:
+//var allLower = userInput.toLowerCase();
+
+//Question 2:
+// var x = "Jawan Pakistan";
+// x = x.toLowerCase(); 
+// alert(x);
+
+// //Question3:
+
+// var y = "Metaverse "; 
+// y = y.toUpperCase(); 
+// console.log(y); 
+
+//Question4:
+
+// var x="Sahil khan";
+//  var y=x.toLowerCase();
+// alert(y);
+
+//Question 5:
+// var myArray=["WEB","SCRIPT"];
+// var z= myArray[0].toLowerCase(); 
+// alert(z);
+
+
+// Question6:
+// var city="islamabad";
+// city=city.toLocaleUpperCase();
+// alert(city);
+
+// Question7:
+// var cityName = "kaRacHi";
+// cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+// alert(cityName);
+
+
+
+//CHAPTER 22-25:
+
+
+//Question1:
+// var sameWords = "captain";
+// sameWords = sameWords.slice(0, 1) + sameWords.slice(3);
+// alert(sameWords);
+
+
+//Quwstion2:
+
+// var myString = "Hello,World";
+// var numberOfCharacters = myString.length;
+
+// alert(numberOfCharacters);
+
+//Question3:
+
+// var animal = "elephant";
+// var startIndex = Math.floor(animal.length / 2) - 2;
+// var endIndex = startIndex + 4;
+
+// var seg = animal.slice(startIndex, endIndex);
+
+// alert(seg);
+
+//Question4:
+
+// var MyName="Sahil Khan";
+// var MyNamelength=MyName.length;
+// alert(MyNamelength);
+
+//Question5:
+// var myString = "This is my page";
+// var numberOfCharacters = myString.length; 
+
+// var slicedString = myString.slice(1, -3);
+
+// console.log(numberOfCharacters); 
+// console.log(slicedString);
+
+
+//Question6:
+// 	var text = "I Live in Karachi.";
+// var indx = text.indexOf("Live");
+// alert(indx);
+
+//Quwstion7:
+// 	var text = "I not Live Karachi.";
+// var indx = text.lastIndexOf("be");
+// alert(indx);
+
+
+// Question8:
+// var text = "Let's go for a walk. I love to go hiking and explore the mountains. Let's go together!";
+// var lastIndex = text.lastIndexOf("go");
+
+// alert(lastIndex);
+
+// Question10:
+// var myString = "abcde";
+// var characterAtIndex2 = myString.charAt(2);
+// console.log(characterAtIndex2);
+
+//Question11:
+// var text  ="This is Assignment of course Metaverse";
+// var char=text.charAt(9);
+// alert(char);
+
+
+//Question12:
+// var str = "WorldIsBULF";
+// var x = str.charAt(str.length - 1);
+
+// console.log(x)
+
+//Question 13:
+// var input="Sahil";
+// var char=input.charAt(4);
+// alert(char);
+
+// Question 14:
+
+// var yourStringVariable="Sahil"
+// if (yourStringVariable.charAt(2) === 'Sahil') {
+//     alert(yourStringVariable);
+//   } else {
+//     alert("wrong equation");
+//   }
+
+// Question 15:
+// var charArray = [];
+
+// var reply = "no, no now, , no way";
+
+// for (var i = 0; i < reply.length; i++) {
+
+//   charArray.push(reply.charAt(i));
+// }
+
+
+// var revisedReply = charArray.join('');
+
+// revisedReply = revisedReply.replace("no", "yes");
+
+// console.log(revisedReply); 
+
+
+//Chapter26:
+//Question1:
+// var originalNumber = 5.75; 
+// var roundedNumber = Math.round(originalNumber);
+
+// console.log(roundedNumber);
+
+//Question2:
+
+// var origNum = 5.2;
+// var roundNum = Math.ceil(origNum);
+
+// console.log(roundNum); 
+
+//Question3:
+// var origNum = 5.8;
+// var roundNum = Math.floor(origNum);
+
+// console.log(roundNum);
+
+//Question4:
+// var firstnunber=6.7;
+// var roundnumber=Math.round(firstnunber);
+// alert(roundnumber);
+
+//Question5:
+// var origNum = 0.5;
+// var roundNum = Math.floor(origNum);
+
+// console.log(roundNum);
+
+
+//Chapters 27:
+
+var random = Math.random();
+var scaledRandom = Math.floor(random * 50) + 1;
+
+alert(scaledRandom); 
+
+
+
+
